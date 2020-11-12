45f920a743e4aa53bc4175b519fbc12c2f248793
Luiz Wynne <luiz@fintechjobs.ie>
2020-11-12 16:12:57 +0100

    small adjust

--- client/src/components/layouts/Alert.js
+++ client/src/components/layouts/Alert.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 
 const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
-    <div key={alert.id} className={`alert alert-${alert.type}`}>
+    <div key={alert.id} className={`alert alert-${alert.alert_type}`}>
         { alert.msg }
     </div>
 ));
