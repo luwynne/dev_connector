b0711a9c0423aafd2220ce5350d56b6945545a40
Luiz Wynne <luiz@fintechjobs.ie>
2020-11-13 15:39:30 +0100

    FE in progress

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layouts/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -36,6 +38,7 @@ const App = () => {
                 <Switch>
                   <Route exact path="/register" component={Register}/>
                   <Route exact path="/login" component={Login}/>
+                  <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                 </Switch>
               </section>
         </Fragment>
